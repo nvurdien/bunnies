85fa84d55fdb0ff24a7ea3715956ab54a278d30a
Christopher Grant <christophergrant@openmailbox.org>
2017-12-12 03:29:26 -0800

    rabbit-> image, consolidate schema, one-to-many

--- bunnies/models/table.sql
+++ bunnies/models/table.sql
@@ -1,3 +1,12 @@
-CREATE TABLE IF NOT EXISTS rabbit (
-    hash binary(16) 
+CREATE TABLE IF NOT EXISTS image (
+    hash binary(16) NOT NULL,
+    fullpath varchar NOT NULL,
+    timestamp smalldatetime,
+    PRIMARY KEY (hash)
+)
+
+CREATE TABLE IF NOT EXISTS breed (
+    hash binary(16) NOT NULL,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY (hash)
 )
