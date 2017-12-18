fd5b8606f3a90b6ebc5553bded9d03828305264b
christopher grant <christopher.grant@csu.fullerton.edu>
2017-12-18 05:41:03 -0800

    start of upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_recaptcha import ReCaptcha
 app = Flask(__name__, static_url_path='')
+recaptcha = ReCaptcha(app=app)
 
 @app.route('/')
 def index():
@@ -12,6 +14,16 @@ def breeds():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/upload')
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        rec = Photo(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Ok")
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run()
