590c2b9e5e11c0a7093953e3d890179e3effe4f7
Christopher Grant <christophergrant@openmailbox.org>
2017-12-20 13:40:36 -0800

    sqlite3 -> mariadb

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Bunnynet is a democratic rabbit breed classification platform.
 
 
-
 ## Platform
 
 Flask
@@ -16,13 +15,14 @@ Python3.6
 ## TODO
 
 * Hand-crafted autocomplete (probably using Levenshtein distance)
-* Allow user upload
+* Allow user upload (recaptcha?)
 * Deep learning integration for predictions
 * Prediction API
 
 ## Design choices:
 
 * Image -> vote many-to-one, better than one-to-one, no extremely large and  sparse objects. 
+* MD5 isn't secure, but we don't think that it matters for this usecase. MD5 is pretty fast, so we go with the fastest algorithm.
 
 
 ### But seriously
--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
 from flask_recaptcha import ReCaptcha
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
 app = Flask(__name__, static_url_path='')
 recaptcha = ReCaptcha(app=app)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -24,18 +33,6 @@ def upload():
         flash("Ok")
         return redirect(url_for('show', id=rec.id))
     return render_template('upload.html')
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
 
 if __name__ == '__main__':
     app.run()
